75beeeca63365f80d088a8ec8726633ed75737d4
Soumya Satheesan <25707177+soumya225@users.noreply.github.com>
2021-04-21 13:47:45 +0400

    Custom card layout and ArrayAdapter
    - created logo
    - changed layout of cards using CardView
    - created custom MovieAdapter
    - created action such that clicking on card will open IMdB URL of movie

--- app/src/main/java/com/example/movietinder/MainActivity.java
+++ app/src/main/java/com/example/movietinder/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,14 +19,19 @@ import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 import java.util.ArrayList;
 import java.util.List;
 
+//TODO: add info for users to indicate how to swipe (e.g. click to open website)
+
 public class MainActivity extends AppCompatActivity {
 
     private Button signOutButton;
-
-    private ArrayList<String> al = new ArrayList<>();
-    private ArrayAdapter<String> arrayAdapter;
+    private MovieAdapter movieAdapter;
 
     private FirebaseAuth firebaseAuth;
+
+    private final String IMdB_BASE_URL = "https://www.imdb.com/title/";
+
+    private ListView listView;
+    private List<Movie> cardItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         firebaseAuth = FirebaseAuth.getInstance();
+
+        cardItems = new ArrayList<>();
 
         signOutButton = findViewById(R.id.sign_out_button);
         signOutButton.setOnClickListener(new View.OnClickListener() {
@@ -48,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
 
         QueryService qs = new QueryService(this);
 
-        qs.getMoviesList(10, 3, "Romance", new QueryService.VolleyResponseListener() {
+        qs.getMoviesList(15, 3, "Romance", new QueryService.VolleyResponseListener() {
             @Override
             public void onError(String message) {
                 makeToast(MainActivity.this, message);
@@ -58,25 +67,25 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(List<Movie> movieList) {
 
                 for(int i = 0; i < movieList.size(); i++) {
-                    al.add(movieList.get(i).toString());
+                    cardItems.add(movieList.get(i));
                 }
 
-                arrayAdapter.notifyDataSetChanged();
+                movieAdapter.notifyDataSetChanged();
             }
         });
 
-        arrayAdapter = new ArrayAdapter<>(MainActivity.this, R.layout.item, R.id.helloText, al);
+        movieAdapter = new MovieAdapter(MainActivity.this, R.layout.item, cardItems);
 
         SwipeFlingAdapterView flingContainer = findViewById(R.id.frame);
 
-        flingContainer.setAdapter(arrayAdapter);
+        flingContainer.setAdapter(movieAdapter);
         flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
             @Override
             public void removeFirstObjectInAdapter() {
                 // this is the simplest way to delete an object from the Adapter (/AdapterView)
                 Log.d("LIST", "removed object!");
-                al.remove(0);
-                arrayAdapter.notifyDataSetChanged();
+                cardItems.remove(0);
+                movieAdapter.notifyDataSetChanged();
             }
 
             @Override
@@ -105,13 +114,32 @@ public class MainActivity extends AppCompatActivity {
         flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
             @Override
             public void onItemClicked(int itemPosition, Object dataObject) {
-                makeToast(MainActivity.this, "Clicked!");
+                //TODO: correct movie is not being retrieved - currentMovie is always first movie
+
+                Movie currentMovie = movieAdapter.getItem(itemPosition);
+                String IMdBCode = currentMovie.getImdb_code();
+                String urlToOpen = IMdB_BASE_URL + IMdBCode;
+
+                openIMdBLink(urlToOpen);
+
+                makeToast(MainActivity.this, "Opening IMdB web page for " + currentMovie.getTitle());
             }
         });
     }
 
     static void makeToast(Context ctx, String s){
         Toast.makeText(ctx, s, Toast.LENGTH_SHORT).show();
+    }
+
+    void openIMdBLink(String url) {
+        // Convert the String URL into a URI object (to pass into the Intent constructor)
+        Uri movieUri = Uri.parse(url);
+
+        // Create a new intent to view the movie URI
+        Intent websiteIntent = new Intent(Intent.ACTION_VIEW, movieUri);
+
+        // Send the intent to launch a new activity
+        startActivity(websiteIntent);
     }
 
 
---
+++ app/src/main/java/com/example/movietinder/MovieAdapter.java
@@ -0,0 +1,37 @@
+package com.example.movietinder;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MovieAdapter extends ArrayAdapter<Movie> {
+    Context context;
+
+    public MovieAdapter(Context context, int resourceID, List<Movie> items) {
+        super(context, resourceID, items);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        Movie currentMovie = getItem(position);
+
+        if(convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item, parent, false);
+        }
+
+        TextView title = convertView.findViewById(R.id.movie_info);
+        ImageView poster = convertView.findViewById(R.id.movie_poster);
+
+        title.setText(currentMovie.toString());
+        poster.setImageResource(R.drawable.ic_launcher_foreground);
+
+        return convertView;
+    }
+}
